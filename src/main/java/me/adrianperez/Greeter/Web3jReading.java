72ee8584db48b64d8ef47497a895da2c620afa53
Adrian Perez <me@adrianperez.me>
2022-03-30 17:31:31 +0200

    Count amount of blocks including the genesis block

--- src/main/java/me/adrianperez/Greeter/Web3jReading.java
+++ src/main/java/me/adrianperez/Greeter/Web3jReading.java
@@ -4,6 +4,7 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
+import java.math.BigInteger;
 
 public class Web3jReading {
 
@@ -12,6 +13,9 @@ public class Web3jReading {
         var web3 = Web3j.build(new HttpService("http://localhost:7545"));
 
         var version = web3.web3ClientVersion().send();
-        System.out.println(version.getWeb3ClientVersion());
+        System.out.println("Blockchain: " + version.getWeb3ClientVersion());
+
+        var blockNumber = web3.ethBlockNumber().send();
+        System.out.println("Number of blocks: " + (blockNumber.getBlockNumber().add(BigInteger.valueOf(1))));
     }
 }
